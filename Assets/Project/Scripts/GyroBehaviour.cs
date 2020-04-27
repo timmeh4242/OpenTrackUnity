3e4a7eaae264feb499b2d9477e2f4d0474da0031
tim <tim@finergames.com>
2020-04-26 18:57:24 -0700

    - initial commit

---
+++ Assets/Project/Scripts/GyroBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Linq;
+using System;
+
+public class GyroBehaviour : MonoBehaviour
+{
+    public Transform Target;
+    Gyroscope gyro => Input.gyro;
+
+    UdpClient udpClient;
+
+    string OPENTRACK_IP = "127.0.0.1";
+    int OPENTRACK_PORT = 4242;
+
+    void OnEnable()
+    {
+        var ipEndPoint = new IPEndPoint(IPAddress.Parse(OPENTRACK_IP), OPENTRACK_PORT);
+        udpClient = new UdpClient();
+        udpClient.Connect(ipEndPoint);
+    }
+
+    void OnDisable()
+    {
+        udpClient?.Close();
+    }
+
+    void Update()
+    {
+        if (gyro.enabled)
+        {
+            Target.rotation = gyro.attitude;
+        }
+        var byteArray = BuildMessage(Target.eulerAngles);
+        udpClient.Send(byteArray, byteArray.Length);
+    }
+
+    private byte[] BuildMessage(Vector3 orientation)
+    {
+        return new double[]
+        {
+                0,
+                0,
+                0,
+                (double)orientation.x,
+                (double)orientation.y,
+                (double)orientation.z
+        }.SelectMany(BitConverter.GetBytes).ToArray();
+    }
+}
